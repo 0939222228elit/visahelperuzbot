29dda73ec849736256e635f06221167c843fa225
0939222228elit <0939222228elit@gmail.com>
2025-04-29 02:18:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("7485677471:AAHXNl3oDsVaE1QOSvFT2eyuy0-1lpSWxDg")
-ADMIN_ID = int(os.getenv("7406187939"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
